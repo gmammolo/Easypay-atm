08f12f1c307f0c6b4b8c102e6767b0728effad9d
Giuseppe Mammolo <gmammolo@gmail.com>
2021-02-01 20:33:26 +0100

    fix(placeholder ATM)

--- src/app/features/movements/pipes/atm-name.pipe.ts
+++ src/app/features/movements/pipes/atm-name.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UtenteService } from 'src/app/core/services/utente.service';
 @Pipe({
@@ -15,9 +15,11 @@ export class AtmNamePipe implements PipeTransform {
     if (!idAtm) {
       return null;
     }
-    return this.utenteService.getUtenteNoSecurity(idAtm).pipe(
-      map(utente => utente.ragSoc )
-    );
+    // return this.utenteService.getUtenteNoSecurity(idAtm).pipe(
+    //   map(utente => utente.ragSoc )
+    // );
+    // TODO: attualmente c'è un unico atm
+    return of('ATM01');
   }
 
 }
