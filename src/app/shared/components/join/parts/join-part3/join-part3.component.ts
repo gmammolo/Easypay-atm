3ec87ecb189429095417ea5437b9fb5264e6ecad
Giuseppe Mammolo <gmammolo@gmail.com>
2021-02-01 14:37:00 +0100

    add snackbar successo registrazione

--- src/app/shared/components/join/parts/join-part3/join-part3.component.ts
+++ src/app/shared/components/join/parts/join-part3/join-part3.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
 import { RoutingService } from 'src/app/core/services/routing.service';
 import { UtenteService } from 'src/app/core/services/utente.service';
@@ -23,7 +24,13 @@ export class JoinPart3Component implements OnInit, OnDestroy {
 
   private subscriptions: Subscription[] = [];
 
-  constructor(private utenteService: UtenteService, private routingService: RoutingService, private ngZone: NgZone) { }
+  private readonly toastConfig = {
+    horizontalPosition: 'center' as MatSnackBarHorizontalPosition,
+    verticalPosition: 'top' as MatSnackBarVerticalPosition,
+    panelClass: 'toast-success',
+  };
+
+  constructor(private utenteService: UtenteService, private routingService: RoutingService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.subscriptions.push(
@@ -31,7 +38,9 @@ export class JoinPart3Component implements OnInit, OnDestroy {
       .subscribe({
         next: () => {
           this.status = StatusEnum.success;
-          setInterval(() => this.ngZone.run(() =>  this.routingService.gotoHome()), 2000);
+          this.snackBar.open('Account creato correttamente!', 'ok', this.toastConfig);
+          this.routingService.gotoHome();
+          // setInterval(() => this.ngZone.run(() =>  this.routingService.gotoHome()), 2000);
         },
         error: () => this.status = StatusEnum.failed
       }));
